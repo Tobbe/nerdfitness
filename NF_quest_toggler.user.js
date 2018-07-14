97e59fbd183c38d37632441a5550357a38002e43
Tobbe Lundberg <tobbe@tlundberg.com>
2018-07-14 22:38:28 +0200

    Show/hide completed quests

---
+++ NF_quest_toggler.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Nerd Fitness completed quests toggler
+// @namespace    https://github.com/tobbe
+// @version      0.1
+// @description  Toggles showing completed quests on the quest page
+// @author       Tobbe
+// @match        https://www.nerdfitness.com/level-up/my-quests/
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        #toggle-completed-quests-container {
+            display: inline-block;
+        }
+
+        #toggle-completed-quests-container input {
+            margin: 0;
+        }
+
+        #toggle-completed-quests-container label {
+            margin: 0;
+        }
+    `);
+
+    function htmlToElement(html) {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        return template.content.firstChild;
+    }
+
+    function insertToggle() {
+        const innerCont = document.querySelectorAll('.fx-inner-cont')[0];
+        const toggleHtml = `
+            <div id="toggle-completed-quests-container">
+                <input type="checkbox" id="toggle-completed-quests" checked>
+                <label for="toggle-completed-quests">Show completed</label>
+            </div>
+        `;
+        const toggle = htmlToElement(toggleHtml);
+        innerCont.insertBefore(toggle, innerCont.firstChild);
+        const checkbox = document.getElementById('toggle-completed-quests');
+        checkbox.onclick = event => showCompleted(event.target.checked);
+    }
+
+    function showCompleted(shouldShow) {
+        const questBlocks = document.querySelectorAll('.quests-quests-block');
+
+        for (const block of questBlocks) {
+            const completedStatus = block.querySelector('.qh-rect img');
+            if (completedStatus.src.endsWith('92.png')) {
+                block.style.display = shouldShow ? 'block' : 'none';
+            }
+        }
+    }
+
+    unsafeWindow.jQuery(document).ajaxComplete((event, jqXHR, ajaxOptions) => {
+        const toggle = document.getElementById('toggle-completed-quests');
+        showCompleted(toggle.checked);
+    });
+
+    insertToggle();
+})();
